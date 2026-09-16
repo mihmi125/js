af73b843878ac0042909b5c34d9c4811907a7e7f
mihmi125 <49343781+mihmi125@users.noreply.github.com>
2026-09-16 06:03:30 +0000

    idk

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,27 +1,18 @@
-import ExpenseDate from './ExpenseDate'
-import './ExpenseItem.css'
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
-
-    const day =props.data.date.toLocaleString('en-US', {day: '2-digit'})
-    const month =props.data.date.toLocaleString('en-US', {month: 'long'})
-    const year =props.data.date.getFullYear()
-    
-        
-
-    return (
-        <div className='expense-item'>
-                        <div className='expense-date'>
-                <div className='expense-date__month'>{month}</div>
-                <div className='expense-date__day'>{day}</div>
-                <div className='expense-date__year'>{year}</div>
-            </div>
-            <div className='expense-item__description'>
-                <h2>{props.data.title}</h2>
-                <div className='expense-item__price'>{props.data.price}</div>
-            </div>
-        </div>
-    )
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.data.date} />
+      <div className="expense-item__description">
+        <h2>{props.data.title}</h2>
+        <div className="expense-item__price">${props.data.price}</div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 import './Expenses.css';
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.data.map((item, index) => (
         <ExpenseItem key={index} data={item} />
       ))}
-    </div>
+    </Card>
   );
 }
 
